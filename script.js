812c607e204849b55c47de8baa0c7c9579430267
Berresu <90646787+Berresu@users.noreply.github.com>
2022-09-19 14:41:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-let button=document.getElementById("btnHesapla");
+let number=parseInt(prompt());
+let sayi1=0;
+let sayi2=1;
+let sonrakiSayi;
+let button=document.getElementById("btnFibonacci");
+let sonuc=document.getElementById("sonuc");
+
 button.addEventListener("click", ()=>{
-    let sonuc=document.getElementById("sonuc");
-    let sayi1=0;
-    let sayi2=1;
-    let sayiFibonacci;
-  
-    for(let i=1; i<=144; i++){
-      console.log(sayi1);
-      sayiFibonacci=sayi1+sayi2;
-      sayi1=sayi2;
-      sayi2=sayiFibonacci;
-      sonuc.innerHTML=sayiFibonacci;
+    for(let i=1; i<=number; i++){
+        console.log(sayi1);
+        sonrakiSayi=sayi1+sayi2;
+        sayi1=sayi2;
+        sayi2=sonrakiSayi;
+        sonuc.innerHTML=sonrakiSayi;
     }
 });
